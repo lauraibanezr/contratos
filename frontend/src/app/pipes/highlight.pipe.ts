3db4e7210b2b211dfe6c8dc7e015e53008f9a664
Laura <laura@servicex.cu>
2021-04-05 17:21:10 -0400

    Highlight the result

--- frontend/src/app/pipes/highlight.pipe.ts
+++ frontend/src/app/pipes/highlight.pipe.ts
@@ -12,15 +12,17 @@ export class HighlightSearch  implements PipeTransform {
         let splitQuery = args.split(re);
         let numQueries = splitQuery.length;
         let returnedHighlighted: String = value; 
-        console.log(`number of queries: ${numQueries}`)
+        //console.log(`number of queries: ${numQueries}`)
         for (var i = 0; i < numQueries; i++){
 
-          console.log(`${i}: ${splitQuery[i]}`)
+       //   console.log(`${i}: ${splitQuery[i]}`)
           let startIndex = value.toUpperCase().indexOf(splitQuery[i].toUpperCase());
           if (startIndex != -1) {
                 let endLength = args.length;
                 let matchingString = value.substr(startIndex, endLength);
-                returnedHighlighted = returnedHighlighted.replace(matchingString, "<mark>" + matchingString + "</mark>");
+                // returnedHighlighted = returnedHighlighted.replace(matchingString, "<mark>" + matchingString + "</mark>");
+                // value = value.replace(reText, "<span class='highlight-search-text'>" + text + "</span>"); 
+                returnedHighlighted = returnedHighlighted.replace(matchingString, "<span class='highlight-search-text'>" + matchingString + "</span>" )
            }  
         }
         return returnedHighlighted
